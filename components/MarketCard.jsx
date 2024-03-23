f56402d3ab5ae203d2f655f9fea70406052d9373
shubh1712 <shubhhhhhj17@gmail.com>
2024-03-23 20:29:14 +0530

    changed image 3

--- components/MarketCard.jsx
+++ components/MarketCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import img from "../components/pngs/bgimg.jpeg"
+import img from "../components/pngs/market.png"
 import Image from 'next/image';
 
 function MarketCard({marketData}) {
 
     const{title , description} = marketData
     return ( 
-    <div className='m-2 rounded-xl card w-full p-2  border-orange-500  border-2 '>
-        <div>
+    <div className='m-2 rounded-lg card w-full   border-orange-500  border-2 '>
+        <div >
             <Image src={img} alt="img" className='rounded-lg' /> 
         </div>
-        <p className='font-bold	'>
+        <p className='font-bold p-2	'>
             {title}
         </p>
         <hr />
-        <p>
+        <p className="p-2">
             {description}
         </p>
     </div>
